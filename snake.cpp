89d8e723737f021906fde62cf3117fcbb888af68
Wexemm <gamerklanstudio@gmail.com>
2025-05-01 12:42:41 +0200

    pontstruktúra

--- snake.cpp
+++ snake.cpp
@@ -7,4 +7,13 @@
 // Ablak és rácsméret konstansai
 const int WIDTH = 640;
 const int HEIGHT = 480;
-const int CELL_SIZE = 20;
+const int CELL_SIZE = 20; // minden "mező" 20x20 pixel
+
+// Irányokat reprezentáló enum
+enum Direction { UP, DOWN, LEFT, RIGHT };
+
+// Egyszerű pont struktúra a kígyótagok és alma helyének tárolásához
+
+struct Point {
+    int x, y;
+};
