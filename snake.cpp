ff21169b212673cbca6c009e733fd4896f6ca144
Wexemm <gamerklanstudio@gmail.com>
2025-05-01 12:41:21 +0200

    Ablak és rácsméret konstansai

--- snake.cpp
+++ snake.cpp
@@ -1,4 +1,10 @@
+// Alap C++ és SDL2 fejlécek
 #include <SDL2/SDL.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+
+// Ablak és rácsméret konstansai
+const int WIDTH = 640;
+const int HEIGHT = 480;
+const int CELL_SIZE = 20;
